fb1225ae3d4a9b27dc0b926ba1f91c2bff0bfc1d
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2015-06-20 14:43:21 +0200

    Some modifications to use db.sql as database init script on startup.

--- middlewares/database.rs
+++ middlewares/database.rs
@@ -9,14 +9,14 @@ pub fn register() -> PostgresMiddleware {
     // postgres
     let pg = PostgresMiddleware::new(DB_ADDRESS);
     
-    //load db.sql in the first place
+    // init database using db.sql
     let mut f = File::open("db.sql").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
 
     {
         let conn = pg.pool.get().unwrap();
-        conn.execute(&s[..], &[]).unwrap();
+        conn.batch_execute(&s[..]).unwrap();
     }
 
     pg
