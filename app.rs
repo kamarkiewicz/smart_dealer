bc3c1fd8a1d344417dd8e54fc0ddf25e42513e9c
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2015-06-27 14:09:09 +0200

    still does not working

--- app.rs
+++ app.rs
@@ -4,6 +4,7 @@
 extern crate iron;
 extern crate router;
 extern crate rustc_serialize;
+extern crate postgres;
 extern crate iron_postgres_middleware as ipm;
 extern crate time;
 extern crate handlebars_iron as hbs;
